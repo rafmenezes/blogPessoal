1c141fcc5e331f49585f02a12833f38b367b3cc3
rafmenezes <rafael.mds92@gmail.com>
2021-08-17 20:50:33 -0300

    Atualizando API BlogPessoal Swagger/Heroku

--- src/main/java/org/generation/blogPessoal/model/UserLogin.java
+++ src/main/java/org/generation/blogPessoal/model/UserLogin.java
@@ -1,14 +1,43 @@
 package org.generation.blogPessoal.model;
 
-public class UserLogin {
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+public class UserLogin {
+	
+	private long id;
+	
 	private String nome;
+	
+	@NotBlank(message = "Necessario Email")
+	@Email(message = "Necessario Email")
+	private String email;
 	
 	private String usuario;
 	
+	@Size(min = 5, max = 100, message = "Necessario min 5 caracteres")
 	private String senha;
 	
 	private String token;
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 	public String getNome() {
 		return nome;
--- src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -20,6 +20,9 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter
 	@Override
 	protected void configure (AuthenticationManagerBuilder auth) throws Exception
 	{
+		auth.inMemoryAuthentication()
+		.withUser("admin").password(passwordEncoder().encode("admin")).authorities("ROLE_ADMIN");
+		
 		auth.userDetailsService(userDetailsService);
 	}
 	
@@ -33,6 +36,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter
 	protected void configure (HttpSecurity http) throws Exception
 	{
 		http.authorizeRequests()
+		.antMatchers("/**").permitAll()
 		.antMatchers("/usuarios/logar").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
 		.anyRequest().authenticated()
